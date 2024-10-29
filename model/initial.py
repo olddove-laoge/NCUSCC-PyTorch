38a59de87f9e88f47ea9acb362197b4912471848
老鸽 <lyh102026@126.com>
2024-10-29 19:52:02 +0800

    Update initial.py

--- model/initial.py
+++ model/initial.py
@@ -20,10 +20,10 @@ transform = transforms.Compose([
 
 # 使用官方方式加载数据集
 trainset = torchvision.datasets.CIFAR10(root=data_save_pth, train=True, download=True, transform=transform)
-trainloader = DataLoader(trainset, batch_size=4, shuffle=True, num_workers=2)
+trainloader = DataLoader(trainset, batch_size=32, shuffle=True, num_workers=4)
 
 testset = torchvision.datasets.CIFAR10(root=data_save_pth, train=False, download=True, transform=transform)
-testloader = DataLoader(testset, batch_size=4, shuffle=False, num_workers=2)
+testloader = DataLoader(testset, batch_size=32, shuffle=False, num_workers=4)
 
 # 检查数据集大小
 print(f'Training set size: {len(trainset)}')
@@ -33,21 +33,30 @@ print(f'Test set size: {len(testset)}')
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
+        # 第一个卷积层，输入通道3（RGB图像），输出通道32，卷积核大小3x3
+        self.conv1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)
+        # 池化层，窗口大小2x2，步长2
         self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        # 第二个卷积层，输入通道32，输出通道64，卷积核大小3x3
+        self.conv2 = nn.Conv2d(32, 64, 3, stride=1, padding=1)
+        # 第一个全连接层，输入特征数为64*8*8（因为经过两次池化后，特征图大小减半两次），输出特征数256
+        self.fc1 = nn.Linear(64 * 8 * 8, 256)
+        # 第二个全连接层，输入特征数256，输出特征数10（CIFAR-10数据集的类别数）
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
+        # 应用第一个卷积层和激活函数ReLU
         x = self.pool(F.relu(self.conv1(x)))
+        # 应用第二个卷积层和激活函数ReLU
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
+        # 展平特征图，为全连接层准备
+        x = x.view(-1, 64 * 8 * 8)
+        # 应用第一个全连接层和激活函数ReLU
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        # 应用第二个全连接层
+        x = self.fc2(x)
         return x
+
 
 # 初始化网络和优化器
 net = Net()
@@ -58,6 +67,11 @@ criterion = nn.CrossEntropyLoss()
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 net.to(device)
 criterion.to(device)
+
+
+# 启用 cuDNN 基准测试模式
+torch.backends.cudnn.benchmark = True
+
 
 start_time = time.time()
 
@@ -75,7 +89,7 @@ for epoch in range(10):  # 这里使用2个epoch作为示例
         optimizer.step()
 
         running_loss += loss.item()
-        if i % 2000 == 1999:  # 每2000个小批量打印一次
+        if i % 2000 == 1:  # 每2000个小批量打印一次
             print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
             print(f'Memory Usage: {psutil.Process(os.getpid()).memory_info().rss / (1024 * 1024):.2f} MB')  # 打印内存使用情况
